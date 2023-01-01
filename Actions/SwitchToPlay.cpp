6845bac7de672b964f48c46a363c62b144ab7cfc
Ziad <ziadhfahmy@yahoo.com>
2023-01-01 16:48:53 +0200

    PickAndHide v1.3

--- Actions/SwitchToPlay.cpp
+++ Actions/SwitchToPlay.cpp
@@ -12,6 +12,8 @@ void SwitchToPlay::Execute() {
 	ReadActionParameters();
 	Input* pIn = pManager->GetInput();
 	Output* pOut = pManager->GetOutput();
+	//Clears Recording.
+	//pManager->ClearRecording();
 	//Outputs message to let the kid know that he switched modes.
 	pOut->PrintMessage("Switched to Play Mode.");
 	//Creates the play toolbar and removes anything in the status bar.
